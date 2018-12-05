952087d2738431e7de8cf6d73a9952cb6dd19df9
whatevenisthis <adamhowec@gmail.com>
2018-12-05 12:26:14 -0500

    Starting on index and sentence
    Made a php file called sentence that is an outline

--- 377/index.php
+++ 377/index.php
@@ -7,7 +7,24 @@
 
 
 </head>
+<body>
+
+<form name="myForm" acion="sentence.php" method="post">
+  <select name="Job" size="1">
+    <option value="Data Analyst">Data Analyst</option>
+  </select>
+</form>
+
+
+<?php
+require_once('database.php');
+$conn = new mysqli($hostname, $username, $password, $database);
+if ($conn->connect_errno) {
+  die('ERROR NO DATABSE'):
+}
 
 
 
+?>
+</body>
 </html>
---
+++ 377/sentence.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+  <title>Sentence</title>
+
+</head>
+<body>
+
+
+<?php
+var_dump($_POST);
+
+
+
+
+
+ ?>
+
+</body>
+</html>
