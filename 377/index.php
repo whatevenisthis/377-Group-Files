0e673ee44550d694a37591f0f0a4d70ebacce79f
whatevenisthis <adamhowec@gmail.com>
2018-12-17 15:26:47 -0500

    finalish

--- 377/index.php
+++ 377/index.php
@@ -2,20 +2,16 @@
 <html>
 
 <head>
-<script src="jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="icon" href="https://cdn.vox-cdn.com/uploads/chorus_asset/file/6620863/S1LV7h1_UMD.0.gif">
 <title>iSchool Salary Application</title>
-<h1>Salary database for iSchool undergrads</h1>
-<p>Please select a position from the dropdown below to see a summary and details.</p>
-
-<script>
-function validateForm() {
- return true;
-}
-</script>
 
 </head>
+<div class="header" id="myHeader">
+  <h1>Salary database for iSchool undergrads</h1>
+</div>
 <body>
-
+  <p>Please select a position from the dropdown below to see a summary and details.</p>
 <?php
   require_once('database.php');
   $conn = new mysqli($hostname, $username, $password, $database);
@@ -26,7 +22,7 @@ function validateForm() {
     $jobsRes = $conn->query($joblist);
 
 
-    echo "<form name='myForm' action='' method='post' onsubmit='return validateForm()'>";
+    echo "<form name='myForm' action='' method='post'>";
     echo "<select name='Job' size='1'>";
     while ($row = $jobsRes->fetch_assoc()) {
       echo "<option value='{$row['joblist']}'>{$row['joblist']}</option>";
@@ -57,14 +53,14 @@ function validateForm() {
       $max = $maxRow["max"];
 
 
-      echo $job, "'s make on average $", $avg, " a year. The minimum is $", $min, " and the maximum is $", $max, ".";
-      echo nl2br("\n\nThe following table shows details about people with this title. \n\n");
+      echo "<p>", $job, "'s make on average $", $avg, " a year. The minimum is $", $min, " and the maximum is $", $max, ".</p>";
+      echo nl2br("\n<p>The following table shows details about people with this title.</p> \n");
 
 
       $table = "SELECT salary AS 'Salary', degree_level AS 'Education Level', experience AS 'Years of Experience' from mydb.Instances INNER JOIN mydb.Jobs on mydb.Instances.job_id = mydb.Jobs.job_id INNER JOIN Degree on mydb.Instances.degree_id = mydb.Degree.degree_id WHERE job_title='$job' ORDER BY salary";
       $tableRes = $conn->query($table);
-      echo "<table border='1'>";
-      echo "<tr><td>Salary</td><td>Education Level</td><td>Years of Experience</td><tr>";
+      echo "<table id='table1'>";
+      echo "<tr><th>Salary</th><th>Education Level</th><th>Years of Experience</th><tr>";
       while ($row2 = $tableRes->fetch_assoc()) {
         echo "<tr><td>{$row2['Salary']}</td><td>{$row2['Education Level']}</td><td>{$row2['Years of Experience']}</td><tr>";
       }
@@ -74,13 +70,11 @@ function validateForm() {
 
 ?>
 
-
-
-
-
-
-
-
-
 </body>
+
+<div class="footer">
+  <p>This is red because we support our school.</p>
+</div>
+
+
 </html>
